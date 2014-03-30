c60366d467f0a66bd9324fc34c0faf452dbeaefd
James Hill <oblongmana@gmail.com>
2014-03-31 12:03:01 +1300

    Fix ref to open_documentation - last push broke it

--- SalesforceReference.py
+++ SalesforceReference.py
@@ -1,5 +1,5 @@
 """SublimeSalesforceReference: Quick access to Salesforce Documentation from Sublime Text"""
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __author__ = "James Hill (oblongmana@gmail.com)"
 __copyright__ = "SublimeSalesforceReference: (C) 2014 James Hill. GNU GPL 3."
 __credits__ = ["All Salesforce Documentation is © Copyright 2000–2014 salesforce.com, inc.", "ThreadProgress.py is under the MIT License, Will Bond <will@wbond.net>, and SalesforceReference.py's RetrieveIndexThread method is a derives in part from code under the same license"]
@@ -11,6 +11,10 @@ import webbrowser
 import threading
 from .ThreadProgress import ThreadProgress
 
+# def plugin_loaded():
+#     thread = RetrieveIndexThread(sublime.active_window())
+#     thread.start()
+#     ThreadProgress(thread, 'Retrieving Salesforce Reference Index...', '')
 
 class SalesforceReferenceCommand(sublime_plugin.WindowCommand):
 
@@ -18,12 +22,6 @@ class SalesforceReferenceCommand(sublime_plugin.WindowCommand):
         thread = RetrieveIndexThread(self.window)
         thread.start()
         ThreadProgress(thread, 'Retrieving Salesforce Reference Index...', '')
-
-    def open_documentation(self, reference_index):
-        if(reference_index != -1):
-            base_url= 'http://www.salesforce.com/us/developer/docs/apexcode'
-            webbrowser.open_new_tab(base_url + self.sf_ref_pages_links[reference_index])
-
 
 
 class RetrieveIndexThread(threading.Thread):
@@ -41,6 +39,11 @@ class RetrieveIndexThread(threading.Thread):
         self.window = window
         threading.Thread.__init__(self)
 
+    def open_documentation(self, reference_index):
+        if(reference_index != -1):
+            base_url= 'http://www.salesforce.com/us/developer/docs/apexcode'
+            webbrowser.open_new_tab(base_url + self.sf_ref_pages_links[reference_index])
+
     def run(self):
         sf_xml = urllib.request.urlopen('http://www.salesforce.com/us/developer/docs/apexcode/Data/Toc.xml').read().decode('utf-8')
         sf_tree = ElementTree.fromstring(sf_xml)
@@ -51,4 +54,4 @@ class RetrieveIndexThread(threading.Thread):
             self.sf_ref_pages_titles.append(parent.attrib['Title'])
             self.sf_ref_pages_links.append(parent.attrib['Link'])
 
-        self.window.show_quick_panel(self.sf_ref_pages_titles, SalesforceReferenceCommand.open_documentation)
+        self.window.show_quick_panel(self.sf_ref_pages_titles, self.open_documentation)
