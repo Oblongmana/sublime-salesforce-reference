c4cb7c008e50c4ec4e3fb86b341b13ce1e07baea
James Hill <oblongmana@gmail.com>
2015-06-02 21:12:27 +1200

    Modify ToC retrieval to use page scraping
    Salesforce has done away with the publically accessible xml Table of
    Contents files we previously utilised to work out what articles to
    present and where to link to.
    
    Thankfully, the html on page is programmatically structured (see
    https://github.com/forcedotcom/doc-portal/blob/0d46efa2b5b0f178879e00a90594f7a294d9a57a/helpers/create_toc.xsl
    by way of illustration - I believe this is the project used for
    generating the docs. To further back that up, it appears that the Table
    of Contents is not publically exposed, being retrieved by a database
    call in routing in a `get_toc` server-side method in https://github.com/forcedotcom/doc-portal/blob/0d46efa2b5b0f178879e00a90594f7a294d9a57a/helpers/helpers.rb)
    
    As such, we can programmatically deconstruct it, unfortunately we just
    have to retrieve all of the cruft that comes with an html page intended
    for use in by a user in a browser (instead of a neat XML file intended
    for programmatic consumption)

--- SalesforceReference.py
+++ SalesforceReference.py
@@ -9,14 +9,79 @@ import urllib
 import xml.etree.ElementTree as ElementTree
 import webbrowser
 import threading
+import collections
 from .ThreadProgress import ThreadProgress
 
-class SalesforceReferenceCache():
-    def __init__(self):
-        self.sf_ref_pages_titles = []
-        self.sf_ref_pages_links = []    
+# Import BeautifulSoup (scraping library) and html.parser
+#  - Necessary, because as at 2015-06-02 Salesforce no longer uses an XML file
+#    for generating Table of Contents, so we have to scrape a ToC out of the
+#    page itself
+#  - NB: attempting to use html5lib was horrible, due to dependence on six,
+#    which obstinately refused to work.
+#  - Built in html.parser seems perfectly adequate to needs, but if we ever
+#    support ST2, we should check for ST2 and use HTMLParser (if BeautifulSoup
+#    supports)
+#  - NB: bs4 was rebuilt (using 2to3) for Python3; we'd need to include a
+#    Python2 build if we ever support ST2
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), os.path.normpath("lib")))
+from bs4 import BeautifulSoup
+import html.parser
+
+SALESFORCE_DOC_URL_BASE = 'http://developer.salesforce.com/docs/atlas.en-us.apexcode.meta/apexcode/'
+
+class SalesforceReferenceCache(collections.MutableSequence):
+    """
+    A cache of SalesforceReferenceEntry objects, sorted by Title. This order
+    will be maintained throughout append operations
+    """
+    def __init__(self, *data):
+        self.entries = list(data)
+        self.__sort_by_title()
+        self.__determine_titles()
+    @property
+    def titles(self):
+        return self.__titles
+    def __getitem__(self, key):
+        return self.entries[key]
+    def __setitem__(self, key, value):
+        self.entries[key] = value
+        self.__sort_by_title()
+        self.__determine_titles()
+    def __delitem__(self, key):
+        del self.entries[key]
+        self.__determine_titles()
+    def __len__(self):
+        return len(self.entries)
+    def __sort_by_title(self):
+        self.entries.sort(key=lambda cacheEntry: cacheEntry.title)
+    def insert(self, key,val):
+        self.entries.insert(key,val)
+        self.__sort_by_title()
+        self.__determine_titles()
+    def __determine_titles(self):
+        self.__titles = list(map(lambda entry: entry.title,self.entries))
+    """str and repr implemented for debugging"""
+    def __str__(self):
+        return str(self.entries)
+    def __repr__(self):
+        return repr(self.entries)
+
+class SalesforceReferenceCacheEntry(object):
+    def __init__(self,title,url):
+        self.title = title
+        self.url = url
+    """str and repr implemented for debugging"""
+    def __str__(self):
+        return str({'title':self.title,'url':self.url})
+    def __repr__(self):
+        return str({'title':self.title,'url':self.url})
+
+
 
 reference_cache = SalesforceReferenceCache()
+
 
 def plugin_loaded():
     settings = sublime.load_settings("SublimeSalesforceReference.sublime-settings")
@@ -27,7 +92,6 @@ def plugin_loaded():
 
 
 class SalesforceReferenceCommand(sublime_plugin.WindowCommand):
-
     def run(self):
         thread = RetrieveIndexThread(self.window)
         thread.start()
@@ -49,25 +113,33 @@ class RetrieveIndexThread(threading.Thread):
             the documentation list for user selection when done. Defaults to
             true - should open the documentaton list when done
         """
-
         self.window = window
         self.open_when_done = open_when_done
         global reference_cache
         threading.Thread.__init__(self)
 
     def run(self):
-        if not (reference_cache.sf_ref_pages_titles and reference_cache.sf_ref_pages_links):
-            sf_xml = urllib.request.urlopen('http://www.salesforce.com/us/developer/docs/apexcode/Data/Toc.xml').read().decode('utf-8')
-            sf_tree = ElementTree.fromstring(sf_xml)
-            leaf_parents = sf_tree.findall("*[@Title='Reference'].//TocEntry[@DescendantCount='0']..")
-            for parent in leaf_parents:
-                reference_cache.sf_ref_pages_titles.append(parent.attrib['Title'])
-                reference_cache.sf_ref_pages_links.append(parent.attrib['Link'])
-
+        if not (reference_cache.entries):
+            sf_html = urllib.request.urlopen(urllib.request.Request(SALESFORCE_DOC_URL_BASE,None,{'User-Agent': 'Mozilla/5.0'})).read().decode('utf-8')
+            page_soup = BeautifulSoup(sf_html, 'html.parser')
+            reference_soup = page_soup.find_all(text='Reference',class_='toc-text')[0].parent.parent.next_sibling
+            leaf_soup_list = reference_soup.find_all(class_='leaf')
+            header_soup_list = map(lambda leaf: leaf.parent.previous_sibling,leaf_soup_list)
+            unique_header_soup_list = list()
+            for header_soup in header_soup_list:
+                if header_soup not in unique_header_soup_list:
+                    unique_header_soup_list.append(header_soup)
+                    header_data_tag = header_soup.find(class_='toc-a-block')
+                    reference_cache.append(
+                        SalesforceReferenceCacheEntry(
+                            header_data_tag.find(class_='toc-text').string,
+                            header_data_tag['href']
+                        )
+                    )
         if(self.open_when_done):
-            self.window.show_quick_panel(reference_cache.sf_ref_pages_titles, self.open_documentation)
+            self.window.show_quick_panel(reference_cache.titles, self.open_documentation)
 
     def open_documentation(self, reference_index):
         if(reference_index != -1):
             base_url= 'http://www.salesforce.com/us/developer/docs/apexcode'
-            webbrowser.open_new_tab(base_url + reference_cache.sf_ref_pages_links[reference_index])
+            webbrowser.open_new_tab(SALESFORCE_DOC_URL_BASE + reference_cache[reference_index].url)
